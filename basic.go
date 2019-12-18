3723c2aa8e9bbb90a303e88980b2660c5890c11a
Pezhman Kasraee <pezhmankasraee@gmail.com>
2019-12-18 16:43:35 +0100

    add type operand with bug

--- basic.go
+++ basic.go
@@ -1,6 +1,12 @@
 package calc
 
-// Add returns summation of two integers
-func Add(a, b int) int {
-	return a + b
+// OperandPair is a pair of two integer
+type OperandPair struct {
+	A int
+	B int
+}
+
+// Add returns summation of two integers of type OperandPair
+func (o OperandPair) Add() int {
+	return o.A + o.B + 1
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/pezhmankasraee/go-calc
+module github.com/pezhmankasraee/go-calc/v2
 
 go 1.13
